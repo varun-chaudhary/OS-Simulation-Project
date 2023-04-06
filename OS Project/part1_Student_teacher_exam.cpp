93dd07582f02bd179dd1845832c3f35c75cc23e8
Varun Chaudhary <varunc105@gmail.com>
2023-04-06 22:10:49 +0530

    fix now 1 student can do assignment once only

--- OS Project/part1_Student_teacher_exam.cpp
+++ OS Project/part1_Student_teacher_exam.cpp
@@ -10,52 +10,72 @@ bool pen_available = false;
 bool paper_available = false;
 bool question_paper_available = false;
 
+bool std1_done = false;
+bool std2_done = false;
+bool std3_done = false;
+
 void teacher()
 {
-    while (true) // will run infinite time
+
+    while (!std1_done || !std2_done || !std3_done) // will run untill all the students are done with assignmenet
     {
-        // selecting two items at random
-        //  let 0= pen ,1=paper , 2= question paper , rand%3 will give number from 0 to 3
-        int item1, item2;
-        item1 = rand() % 3;
+        bool selected_2_items_successfully = false;
+        int random_choice = 0;
+        random_choice = rand() % 3;
         do
         {
-            item2 = rand() % 3;
-        } while (item2 == item1); // keep doing untill both items are differnt
-        // teacher placing available item on the table
-        cout << "Teacher placed ";
-        if (item1 == 0)
-        {
-            cout << "pen";
-            pen_available = true;
-        }
-        else if (item1 == 1)
-        {
-            cout << "paper";
-            paper_available = true;
-        }
-        else
-        {
-            cout << "question paper";
-            question_paper_available = true;
-        }
-        cout << " and ";
-        if (item2 == 0)
-        {
-            cout << "pen";
-            pen_available = true;
-        }
-        else if (item2 == 1)
-        {
-            cout << "paper";
-            paper_available = true;
-        }
-        else
-        {
-            cout << "question paper";
-            question_paper_available = true;
-        }
-        cout << " on table" << endl;
+            selected_2_items_successfully = false;
+            random_choice = rand() % 3;
+            cout << "Teacher placed ";
+            switch (random_choice)
+            {
+            case 0:
+                cout << "pen and paper on the table\n ";
+                if (std1_done) // checking if student with alternate thing is already done or not
+                {
+                    cout << "but student 1 (with question paper) is already done\n\n\n";
+                    continue;
+                }
+                else
+                {
+                    pen_available = true;
+                    paper_available = true;
+                    selected_2_items_successfully = true;
+                }
+
+                break;
+            case 1:
+                cout << "paper and question paper on the table\n";
+                if (std2_done)
+                {
+                    cout << "but student 2 (with pen) is already done\n\n\n";
+                    continue;
+                }
+                else
+                {
+                    paper_available = true;
+                    question_paper_available = true;
+                    selected_2_items_successfully = true;
+                }
+                break;
+
+            case 2:
+                cout << "pen and question paper on the table\n";
+                if (std3_done)
+                {
+                    cout << "but student 3  (with paper) is already done\n\n\n";
+                    continue;
+                }
+                else
+                {
+                    pen_available = true;
+                    question_paper_available = true;
+                    selected_2_items_successfully = true;
+                }
+
+                break;
+            }
+        } while (!selected_2_items_successfully); // continue the loop untill 2 items are selected succesfuuly
 
         cv.notify_all(); // this will notify(invoke) all other threads cv(conditional vaariable)
                          // and they will check further the condition is satisfied , in short- table is realesed now for  students to give assignment
@@ -65,6 +85,8 @@ void teacher()
         cv.wait(lock); // will wait for student to complete the assignment and notify
         cout << "Teacher collected completed assignment" << endl
              << endl;
+        this_thread::sleep_for(chrono ::seconds(2));
+
         // setting everything unuavailable after comletion
         pen_available = false;
         paper_available = false;
@@ -88,30 +110,38 @@ void student(int id, bool &item1_available, bool &item2_available)
         {
             // Checking individual thing available and printiing on console what things are got to start dong assignment
             cout << "Student " << id << " has taken ";
-            if (pen_available)
+            if (pen_available && paper_available)
             {
-                cout << "pen and ";
+                cout << "pen and paper";
+                std1_done = true;
                 pen_available = false;
-            }
-            if (paper_available)
-            {
-                cout << "paper and ";
                 paper_available = false;
             }
-            if (question_paper_available)
+            else if (paper_available && question_paper_available)
             {
-                cout << "question paper and ";
+                cout << "paper and question paper ";
+                std2_done = true;
+                paper_available = false;
                 question_paper_available = false;
             }
-            cout << "started doing the assignment" << endl;
+            else if (question_paper_available && pen_available)
+            {
+                cout << "pen and question paper ";
+                std3_done = true;
+                question_paper_available = false;
+                pen_available = false;
+            }
+            cout << "and started doing the assignment" << endl;
 
             this_thread::sleep_for(chrono::seconds(5)); // sleep for 5 seconds to stimulate writing an assignment
+            cout << "Student " << id << " completed the assignment" << endl
+                 << endl;
+
             lock.unlock();
             cv.notify_all(); // tellimg teacher assignment is done on completeion as per question
             // this will notify(invoke) all other threads cc(conditional vaariable)
             // and they will check further the condition is satisfied , in short -shared table is realesed now for other student to give assignment
-
-            cout << "Student " << id << " completed the assignment" << endl;
+            pthread_exit(NULL); // student thread exit after doing assignment
         }
         else
         {
@@ -123,11 +153,11 @@ void student(int id, bool &item1_available, bool &item2_available)
 int main()
 {
     // multiple threads of student and teacher is created
-    thread student1(student, 1, ref(pen_available), ref(paper_available));
-    thread student2(student, 2, ref(paper_available), ref(question_paper_available));
-    thread student3(student, 3, ref(pen_available), ref(question_paper_available));
-
+    thread student1(student, 1, ref(pen_available), ref(paper_available));            // have question paper
+    thread student2(student, 2, ref(paper_available), ref(question_paper_available)); // have pen
+    thread student3(student, 3, ref(pen_available), ref(question_paper_available));   // have paper
     thread teacher_thread(teacher);
+
     // join method will hold main fuction till the executions of threads
     student1.join();
     student2.join();
